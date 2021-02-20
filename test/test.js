bd335f65668b61b61efe13b22d4da03323f518b3
ryunakae <ryunakae@gmail.com>
2021-02-20 16:42:39 +0900

    initial commit

---
+++ test/test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const firebase = require('@firebase/testing');
+
+const MY_PROJECT_ID = "nekonarabe-app";
+
+describe("nekonarabe app", () => {
+  
+  it("first test", () => {
+    assert.equal(2+2, 4);
+  });
+})
